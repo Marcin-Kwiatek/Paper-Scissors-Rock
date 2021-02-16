894aac9a8efaad66157b1828b285654270672919
Marcin Kwiatek <kwiatekm4@gmail.com>
2021-02-16 17:27:52 +0100

    adding game stages

--- script.js
+++ script.js
@@ -7,27 +7,32 @@ scissors.addEventListener("click", function () { choose("img/scissors.png") })
 rock.addEventListener("click", function () { choose("img/rock.png") })
 
 function choose(event) {
-    let resultComputer = computer()
-    let imgComputer = document.createElement('img')
-    imgComputer.src = resultComputer
-    document.getElementById('computerChoice').appendChild(imgComputer)
-    let war = document.createElement('img')
-    war.src = event 
-    let war2 = document.createElement('img')
-    war2.src = resultComputer
-    document.getElementById('war').appendChild(war)
-    document.getElementById('war').appendChild(war2)
-
-    let winner = document.getElementById("winner")
-    if (event === resultComputer) {
-        winner.innerHTML = "Draw"
-    }
-    if (event === "img/paper.png" && resultComputer === "img/rock.png" || event === "img/rock.png" && resultComputer === "img/scissors.png" || event === "img/scissors.png" && resultComputer === "img/paper.png") {
-        winner.innerHTML = "Winner = Player"
-    }
-    if (event === "img/paper.png" && resultComputer === "img/scissors.png" || event === "img/scissors.png" && resultComputer === "img/rock.png" || event === "img/rock.png" && resultComputer === "img/paper.png") {
-        winner.innerHTML = "Winner = Computer"
-    }
+    setTimeout(function () {
+        let resultComputer = computer()
+        let imgComputer = document.createElement('img')
+        imgComputer.src = resultComputer
+        document.getElementById('computerChoice').appendChild(imgComputer)
+        setTimeout(function (){
+        let war = document.createElement('img')
+        war.src = event
+        let war2 = document.createElement('img')
+        war2.src = resultComputer
+        document.getElementById('war').appendChild(war)
+        document.getElementById('war').appendChild(war2)
+        setTimeout(function (){
+        let winner = document.getElementById("winner")
+        if (event === resultComputer) {
+            winner.innerHTML = "Draw"
+        }
+        if (event === "img/paper.png" && resultComputer === "img/rock.png" || event === "img/rock.png" && resultComputer === "img/scissors.png" || event === "img/scissors.png" && resultComputer === "img/paper.png") {
+            winner.innerHTML = "Winner = Player"
+        }
+        if (event === "img/paper.png" && resultComputer === "img/scissors.png" || event === "img/scissors.png" && resultComputer === "img/rock.png" || event === "img/rock.png" && resultComputer === "img/paper.png") {
+            winner.innerHTML = "Winner = Computer"
+        }
+    },500)
+    },500)
+    },500)
 }
 function computer() {
     let arrComputer = new Array()
